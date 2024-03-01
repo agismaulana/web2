65b8b9ce0a1b2109db619953f01683118ee38a55
Agis Maulana <agismaulana112@gmail.com>
2024-03-02 01:16:46 +0700

    feat: kommit pertama untuk tugas no 1 dan 2 pada pertemuan 1

---
+++ pertemuan1/tugas1.php
@@ -0,0 +1,8 @@
+<?php
+    print "<center>";
+    print "<font size=16 color=red>BIODATA</font><br>";
+    print "<font size=12 color=blue>Nama : Agis Maulana</font><br>";
+    print "<font size=16 color=blue>Email : agismaulana112@gmail.com</font><br>";
+    print "<font size=16 color=blue>Jurusan : Teknik Informatika</font><br>";
+    print "<font size=16 color=blue>Hobby : Programming</font><br>";
+    print "</center>";
---
+++ pertemuan1/tugas2.php
@@ -0,0 +1,6 @@
+<?php 
+
+    print "<center>";
+    print "<font size=16 color=red>Agis Maulana</font><br><br>";
+    print "<font size=5>Kesan pertama kali belajar php sangat takjub ternyata ada sebuah tulisan yang bisa buat sebuah visualisasi serta mengubah data secara dinamis dan merasa sangat bersemangat untuk bisa serta menguasai bahasa pemrogramannya.</font>";
+    print "</center>";
